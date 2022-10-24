535392966d4ed04ae02fe7e5ce106bc42e1972e1
yousef184 <99906529+yousef184@users.noreply.github.com>
2022-10-24 17:49:09 +0200

    Update saveTransaction.c

--- testFunctions/saveTransaction.c
+++ testFunctions/saveTransaction.c
@@ -10,18 +10,16 @@ ST_accountsDB_t accountsDB[255]=  {{2000.0, RUNNING, "8989374615436851"},
                                    {2000.0, RUNNING, "0114047354192581"}};
 ST_accountsDB_t *ref;
 ST_transaction_t yousef = {{"yousef mohamed mahomud","8989374615436851","11/22"},{12,100,"24/10/2022"},{APPROVED},{RUNNING}};
-ST_transaction_t zeyad = {{"yousef mohamed mahomud","8989374615436852","11/22"},{12,100,"24/11/2022"},{APPROVED},{BLOCKED}};
+ST_transaction_t zeyad = {{"zeyad mohamed mahomud","8989374615436852","11/22"},{12,100,"24/11/2022"},{APPROVED},{BLOCKED}};
 ST_transaction_t transactionsDB[255]={0};
+int sequence =0;
 
-
-void saveTransactionTest(void)
+void listSavedTransactionsTest(void)
 {
-    transactionsDB[0] = zeyad;
-    transactionsDB[1] = yousef;
     for(int i = 0;i<2;i++)
     {
         printf("#########################\n");
-        printf("Transaction Sequence Number: %d\n",i+1);
+        printf("Transaction Sequence Number: %d\n",transactionsDB[i].transactionSequenceNumber+1);
         printf("Transaction Date: %s\n",transactionsDB[i].terminalData.transactionDate);
         printf("New Account Balance: %.2f\n",accountsDB[i].balance);
         printf("Transaction Amount: %.2f\n",transactionsDB[i].terminalData.transAmount);
@@ -34,7 +32,14 @@ void saveTransactionTest(void)
     }
 }
 
-
+void saveTransactionTest(void)
+{
+    transactionsDB[0] = zeyad;
+    transactionsDB[1] = yousef;
+    yousef.transactionSequenceNumber=sequence;
+    zeyad.transactionSequenceNumber=sequence+1;
+    listSavedTransactionsTest();
+}
 int main()
 {
    saveTransactionTest() ;
